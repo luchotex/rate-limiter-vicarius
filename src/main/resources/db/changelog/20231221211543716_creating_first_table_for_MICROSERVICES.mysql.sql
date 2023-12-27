b6dc5bcc9400ea9bb0684d007a5efcdbb2aa4fd1
Luis <luchotex.kuper.android@gmail.com>
2023-12-26 22:24:53 -0400

    -- Removed unnecessary code.

--- src/main/resources/db/changelog/20231221211543716_creating_first_table_for_MICROSERVICES.mysql.sql
+++ src/main/resources/db/changelog/20231221211543716_creating_first_table_for_MICROSERVICES.mysql.sql
@@ -1,5 +1,5 @@
 -- liquibase formatted sql
 
--- changeset Luis Kupferberg:creating first table context
+-- changeset Luis Kupferberg:creating first table
 CREATE TABLE "user" (id varchar(36) NOT NULL, disabled BOOLEAN NOT NULL, first_name VARCHAR(255) NULL, last_login_utc datetime(6) NULL, last_name VARCHAR(255) NULL, CONSTRAINT userPK PRIMARY KEY (id));
 
